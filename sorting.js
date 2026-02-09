8c54243e9106c8d23ebd9b5be6f10901ec688bae
couper117 <ikennykelvin75@gmail.com>
2026-02-09 23:11:36 +0200

    sort

---
+++ sorting.js
@@ -0,0 +1,8 @@
+let students = [
+    { id: 1, name: "Mugisha Yvan", age: 18, gender: "male", grade: 11 },
+    { id: 2, name: "Shimwa Jane", age: 17, gender: "female", grade: 10 },
+    { id: 3, name: "Manzi Smith", age: 19, gender: "male", grade: 11 }
+];
+const sorted = students.sort((a,b) => b.age - a.age);
+const oldest = sorted[0];
+console.log(oldest);
